1544c04d52f227ea3fb3ac41feaabde8409b4953
InsaneXurow10 <104911512+InsaneXurow10@users.noreply.github.com>
2023-03-10 19:19:52 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -8,69 +8,84 @@ import br.com.fiap.appservico.Utils.Verifica;
 // classe do prestador de serviços, que extende a classe Verifica para usar seus métodos de verificação
 public class Usuario extends Verifica {
 
-    // atributos padrão
-    private UUID id;
-    private String nome;
-    private String senha;
-    private String cpf;
-    private int idade;
+	// atributos padrão
+	private UUID id;
+	private String nome;
+	private String senha;
+	private String cpf;
+	private int idade;
+	private ArrayList<ArrayList<String>> publicacoes;
 
-    // método da classe Usuário para criar uma publicação na plataforma
-    public ArrayList<String> publicar(String titulo, String descricao) {
-        ArrayList<String> info = new ArrayList<String>();
-        info.add(titulo);
-        info.add(descricao);
-        return info;
-    }
+	// método da classe Usuário para criar uma publicação na plataforma
+	public ArrayList<String> publicar(ArrayList<ArrayList<String>> array, String titulo, String descricao) {
+		ArrayList<String> info = new ArrayList<String>();
+		info.add(titulo);
+		info.add(descricao);
+		info.add("Id Publicação: " + UUID.randomUUID());
+		array.add(info);
+		return info;
+	}
 
-    // contrutor com atributos
-    public Usuario(String nome, String senha, String cpf, int idade) {
-        id = UUID.randomUUID();
-        this.nome = nome;
-        this.senha = senha;
-        this.cpf = cpf;
-        this.idade = idade;
-    }
+	// contrutor com atributos
+	public Usuario(String nome, String senha, String cpf, int idade) {
+		
+		ArrayList<ArrayList<String>> publicacoes = new ArrayList<ArrayList<String>>();
+		
+		id = UUID.randomUUID();
+		this.nome = nome;
+		this.senha = senha;
+		this.cpf = cpf;
+		this.idade = idade;
+		this.publicacoes = publicacoes;
+	}
 
-    // contrutor padrão
-    public Usuario() {
-        super();
-    }
+	// getters e setters
+	public UUID getId() {
+		return id;
+	}
 
-    // getters e setters
-    public UUID getId() {
-        return id;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public String getSenha() {
+		return senha;
+	}
 
-    public String getSenha() {
-        return senha;
-    }
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
+	public String getcpf() {
+		return cpf;
+	}
 
-    public String getcpf() {
-        return cpf;
-    }
+	public void setcpf(String cpf) {
+		this.cpf = cpf;
+	}
 
-    public void setcpf(String cpf) {
-        this.cpf = cpf;
-    }
+	public int getIdade() {
+		return idade;
+	}
 
-    public int getIdade() {
-        return idade;
-    }
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
+	public ArrayList<ArrayList<String>> getPublicacoes() {
+		return publicacoes;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", senha=" + senha + ", cpf=" + cpf + ", idade=" + idade
+				+ ", getId()=" + getId() + ", getNome()=" + getNome() + ", getSenha()=" + getSenha() + ", getcpf()="
+				+ getcpf() + ", getIdade()=" + getIdade() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+	
 }
