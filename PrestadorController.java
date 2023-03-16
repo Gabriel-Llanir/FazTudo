4cc44193e91fa42e8cc172d98b806b2d5d726fe1
Gabriel Llanir <104911512+InsaneXurow10@users.noreply.github.com>
2023-03-16 10:19:21 -0300

    Update PrestadorController.java

--- PrestadorController.java
+++ PrestadorController.java
@@ -1,51 +1,101 @@
 package br.com.fiap.appservico.Controllers;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import br.com.fiap.appservico.Models.Prestador;
+import br.com.fiap.appservico.Utils.Verifica;
 
 // classe de controle para teste da classe Prestador
 @RestController
-public class PrestadorController {
+public class PrestadorController extends Verifica {
 
-    // contrução de uma classe Prestador
-    Prestador prestador = new Prestador("Gabriel", "Gabriel123", "123.456.789-11", 20, '0');
+    // construção de um array de Prestadores
+    ArrayList<Prestador> prestadores = new ArrayList<Prestador>();
 
-    // função de mapeamento, para colocar na aplicação uma página que exibe o perfil do prestador
-    @GetMapping("/Perfil")
-    public Prestador perfil(){
-        return prestador;
+    // construção de um log
+    Logger log = new LoggerFactory.getLogger(PrestadorController.class);
+
+    //---------------------------------
+// função de mapeamento para colocar, na aplicação, uma página que exibe o perfil do usuário
+    @GetMapping("/Perfil/{id}")
+    public ResponseEntity<Prestador> perfil(@PathVariable Long id){
+        var prestador = prestadores
+                .stream()
+                .filter(d -> d.getId().equals(id))
+                .findFirst();
+
+        if(prestador.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(prestador.get());
     }
 
-    // função de mapeamento para colocar, na aplicação, uma função para atualizar o perfil do prestador
-    @PutMapping("/Perfil")
-    public Prestador perfilPut(@RequestBody Prestador prestador){
-        return prestador;
+    // função de mapeamento para colocar, na aplicação, uma função para atualizar o perfil do usuário
+    @PutMapping("/Perfil/{id}")
+    public ResponseEntity<Prestador> perfilPut(@PathVariable Long id, @RequestBody Prestador prestador) {
+        var prestadorExistente = prestadores
+                .stream()
+                .filter(d -> d.getId().equals(id))
+                .findFirst();
+
+        if(prestadorExistente.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        prestador.setId(id);
+        prestadores.remove(prestadorExistente.get());
+        prestadores.add(prestador);
+
+        return ResponseEntity.ok(prestador);
     }
 
-    // função de mapeamento para colocar, na aplicação, uma função que deleta o perfil do prestador
-    @DeleteMapping("/Perfil")
-    public boolean perfilDel(){
-        return true;
+    // função de mapeamento para colocar, na aplicação, uma função que deleta o perfil do usuário
+    @DeleteMapping("/Perfil/{id}")
+    public ResponseEntity<Prestador> perfilDel(@PathVariable Long id){
+        var prestador = prestadores
+                .stream()
+                .filter(d -> d.getId().equals(id))
+                .findFirst();
+
+        if(prestador.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        prestadores.remove(prestador.get());
+        return ResponseEntity.noContent().build();
     }
-    
+
     //---------------------------------
 
-    // função de mapeamento para colocar, na aplicação, uma página de login do prestador
-    @GetMapping("/Login")
-    public boolean login(@RequestBody String cpf, @RequestBody String senha){
-        return true;
+    // função de mapeamento para colocar, na aplicação, uma página de login do usuário
+    @GetMapping("/Login/{id}")
+    public ResponseEntity<Prestador> login(@PathVariable Long id){
+        var prestador = prestadores
+                .stream()
+                .filter(d -> d.getId().equals(id))
+                .findFirst();
+
+        if(prestador.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(prestador.get());
     }
 
-    // função de mapeamento para colocar, na aplicação, uma página de registro do prestador
+    // função de mapeamento para colocar, na aplicação, uma página de registro do usuário
     @PostMapping("/Registro")
-    public boolean registro(@RequestBody Prestador prestador){
-        return true;
+    public ResponseEntity<Prestador> registro(@RequestBody Prestador prestador){
+        if (verficacpf(prestador.getcpf()) && verificaIdade(prestador.getIdade())){
+            log.info("Registrando usuário: "+prestador);
+            prestadores.add(prestador);
+            return ResponseEntity.status(HttpStatus.CREATED).body(prestador);
+        }
+        else
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
     }
+
 
 }
