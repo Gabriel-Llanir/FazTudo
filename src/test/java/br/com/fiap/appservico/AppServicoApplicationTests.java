3ad0b694084db40c432e0c51d79e8fb1013a4c4f
Gabriel Llanir <104911512+InsaneXurow10@users.noreply.github.com>
2023-03-20 14:34:36 -0300

    Create AppServicoApplicationTests.java

---
+++ src/test/java/br/com/fiap/appservico/AppServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.appservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
