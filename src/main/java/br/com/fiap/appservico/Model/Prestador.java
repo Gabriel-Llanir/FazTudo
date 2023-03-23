de977c93607d4108bd53eee264b5e8b155f27fab
Gabriel Llanir <104911512+InsaneXurow10@users.noreply.github.com>
2023-03-23 02:11:47 -0300

    Update Prestador.java

--- src/main/java/br/com/fiap/appservico/Model/Prestador.java
+++ src/main/java/br/com/fiap/appservico/Model/Prestador.java
@@ -1,84 +1,46 @@
 package br.com.fiap.appservico.Model;
 
-// classe do prestador de serviços, que extende a classe Verifica para usar seus métodos de verificação
+import br.com.fiap.appservico.Post.DadosRegistroPrestador;
+import br.com.fiap.appservico.Put.DadosAtualizacaoPrestador;
+import jakarta.persistence.*;
+import lombok.*;
+
+@EqualsAndHashCode(of = "id")
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="prestadores")
+@Entity(name="Prestador")
 public class Prestador {
 
-    // atributos padrão
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String senha;
     private String cpf;
     private int idade;
-
-    // atributo especial para verificação
     private char status;
+    private Boolean ativo;
 
-    // construtor com atributos
-    public Prestador(Long id, String nome, String senha, String cpf, int idade, char status) {
-        this.id = id;
-        this.nome = nome;
-        this.senha = senha;
-        this.cpf = cpf;
-        this.idade = idade;
-        this.status = status;
+    public Prestador(DadosRegistroPrestador dados) {
+        this.ativo = true;
+        this.nome = dados.nome();
+        this.senha = dados.senha();
+        this.cpf = dados.cpf();
+        this.idade = dados.idade();
     }
 
-    // getters e setters
-    public Long getId() {
-        return id;
+    public void atualizarInformacoes(DadosAtualizacaoPrestador dados) {
+        if (dados.nome()!=null)
+            this.nome = dados.nome();
+        if (dados.senha()!=null)
+            this.senha = dados.senha();
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void excluir() {
+        this.ativo = false;
     }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getcpf() {
-        return cpf;
-    }
-
-    public void setcpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(char status) {
-        this.status = status;
-    }
-
-	@Override
-	public String toString() {
-		return "Prestador [id=" + id + ", nome=" + nome + ", senha=" + senha + ", cpf=" + cpf + ", idade=" + idade
-				+ ", status=" + status + ", getId()=" + getId() + ", getNome()=" + getNome() + ", getSenha()="
-				+ getSenha() + ", getcpf()=" + getcpf() + ", getIdade()=" + getIdade() + ", getStatus()=" + getStatus()
-				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
-				+ "]";
-	}
-    
 }
