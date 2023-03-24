c2e35f732a252fe37fd61aa1f210725e0c9a87f9
Gabriel Llanir <104911512+InsaneXurow10@users.noreply.github.com>
2023-03-24 16:03:22 -0300

    Update DadosMostrarPrestador.java

--- src/main/java/br/com/fiap/appservico/Get/DadosMostrarPrestador.java
+++ src/main/java/br/com/fiap/appservico/Get/DadosMostrarPrestador.java
@@ -2,10 +2,10 @@ package br.com.fiap.appservico.Get;
 
 import br.com.fiap.appservico.Model.Prestador;
 
-public record DadosMostrarPrestador(Long id, String nome, String cpf, int idade, char status, Boolean ativo) {
+public record DadosMostrarPrestador(Long id, String nome, String cpf, String senha, Integer idade, Boolean status, Boolean ativo) {
 
     public DadosMostrarPrestador(Prestador prestador){
-        this(prestador.getId(), prestador.getNome(), prestador.getCpf(), prestador.getIdade(), prestador.getStatus(),
+        this(prestador.getId(), prestador.getNome(), prestador.getCpf(), prestador.getSenha(), prestador.getIdade(), prestador.getStatus(),
                 prestador.getAtivo());
     }
 }
