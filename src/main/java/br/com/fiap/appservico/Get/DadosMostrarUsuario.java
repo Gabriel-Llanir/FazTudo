2737c4e5375370801e4f4448cf082092691fdb57
Gabriel Llanir <104911512+InsaneXurow10@users.noreply.github.com>
2023-03-24 16:04:05 -0300

    Update DadosMostrarUsuario.java

--- src/main/java/br/com/fiap/appservico/Get/DadosMostrarUsuario.java
+++ src/main/java/br/com/fiap/appservico/Get/DadosMostrarUsuario.java
@@ -1,11 +1,10 @@
 package br.com.fiap.appservico.Get;
 
-import br.com.fiap.appservico.Model.Prestador;
 import br.com.fiap.appservico.Model.Usuario;
 
-public record DadosMostrarUsuario(Long id, String nome, String cpf, int idade, Boolean ativo) {
+public record DadosMostrarUsuario(Long id, String nome, String senha, String cpf, Integer idade, Boolean ativo) {
 
     public DadosMostrarUsuario(Usuario usuario) {
-        this(usuario.getId(), usuario.getNome(), usuario.getCpf(), usuario.getIdade(), usuario.getAtivo());
+        this(usuario.getId(), usuario.getNome(), usuario.getSenha(), usuario.getCpf(), usuario.getIdade(), usuario.getAtivo());
     }
 }
