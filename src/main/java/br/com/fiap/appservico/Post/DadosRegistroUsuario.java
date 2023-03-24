4468678a401f484b4001564397a448a925f9e41c
Gabriel Llanir <104911512+InsaneXurow10@users.noreply.github.com>
2023-03-24 15:59:59 -0300

    Update DadosRegistroUsuario.java

--- src/main/java/br/com/fiap/appservico/Post/DadosRegistroUsuario.java
+++ src/main/java/br/com/fiap/appservico/Post/DadosRegistroUsuario.java
@@ -2,10 +2,10 @@ package br.com.fiap.appservico.Post;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Range;
 
 public record DadosRegistroUsuario(@NotBlank String nome,
                                    @NotBlank String senha,
                                    @NotBlank String cpf,
-                                   @NotNull @Pattern(regexp = "\\d{2}") int idade) {
+                                   @NotNull @Range(min = 18, max = 100) Integer idade) {
 }
