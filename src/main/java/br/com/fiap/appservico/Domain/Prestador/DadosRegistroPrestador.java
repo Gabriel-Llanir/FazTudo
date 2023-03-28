3e9914210b27d7d0e6b3412b56bf48b442c2ed0d
Gabriel Llanir <104911512+InsaneXurow10@users.noreply.github.com>
2023-03-27 22:51:37 -0300

    Create DadosRegistroPrestador.java

---
+++ src/main/java/br/com/fiap/appservico/Domain/Prestador/DadosRegistroPrestador.java
@@ -0,0 +1,11 @@
+package br.com.fiap.appservico.Domain.Prestador;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Range;
+
+public record DadosRegistroPrestador(@NotBlank String nome,
+                                     @NotBlank String senha,
+                                     @NotBlank String cpf,
+                                     @NotNull @Range(min = 18, max = 100) Integer idade) {
+}
