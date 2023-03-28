f623ead4257e3138b55354d1ab40d20305a11b95
Gabriel Llanir <104911512+InsaneXurow10@users.noreply.github.com>
2023-03-27 22:55:25 -0300

    Create PublicacaoRepository.java

---
+++ src/main/java/br/com/fiap/appservico/Domain/Publicacao/PublicacaoRepository.java
@@ -0,0 +1,10 @@
+package br.com.fiap.appservico.Domain.Publicacao;
+
+import br.com.fiap.appservico.Utils.PublicacaoPK;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PublicacaoRepository extends JpaRepository<Publicacao, Long> {
+    Optional<Publicacao> findById(PublicacaoPK publicacaoIdObj);
+}
