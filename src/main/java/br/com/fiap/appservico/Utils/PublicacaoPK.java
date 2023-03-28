dc534626b968c97e714e7779eb30f204808b82fc
Gabriel Llanir <104911512+InsaneXurow10@users.noreply.github.com>
2023-03-28 10:52:38 -0300

    Update PublicacaoPK.java

--- src/main/java/br/com/fiap/appservico/Utils/PublicacaoPK.java
+++ src/main/java/br/com/fiap/appservico/Utils/PublicacaoPK.java
@@ -5,36 +5,17 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import java.io.Serializable;
-import java.util.Objects;
 
+@EqualsAndHashCode
 @Getter
 @Setter
-@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
 public class PublicacaoPK implements Serializable {
 
-    @Column(name = "usuId")
     private Long usuarioId;
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-
-    public PublicacaoPK(Long id, Long usuarioId){
-        this.id=id;
-        this.usuarioId=usuarioId;
-    }
-
-    public PublicacaoPK(){}
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PublicacaoPK)) return false;
-        PublicacaoPK that = (PublicacaoPK) o;
-        return Objects.equals(getId(), that.getId()) && Objects.equals(getUsuarioId(), that.getUsuarioId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getUsuarioId());
-    }}
+}
