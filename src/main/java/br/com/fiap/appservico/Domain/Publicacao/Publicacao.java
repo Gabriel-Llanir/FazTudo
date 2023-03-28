f6e0cbe2d7048fd9740094844f2bc6132068926f
Gabriel Llanir <104911512+InsaneXurow10@users.noreply.github.com>
2023-03-27 22:55:08 -0300

    Create Publicacao.java

---
+++ src/main/java/br/com/fiap/appservico/Domain/Publicacao/Publicacao.java
@@ -0,0 +1,46 @@
+package br.com.fiap.appservico.Domain.Publicacao;
+
+import br.com.fiap.appservico.Utils.PublicacaoPK;
+import br.com.fiap.appservico.Domain.Usuario.Usuario;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+@EqualsAndHashCode(of = "id")
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="publicacoes")
+@Entity(name="Publicacao")
+public class Publicacao {
+
+    @EmbeddedId
+    private PublicacaoPK id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+    @JsonBackReference
+    private Usuario usuario;
+
+    private String titulo;
+    private String descricao;
+
+    public Publicacao(DadosRegistroPublicacao dados) {
+        this.titulo = dados.titulo();
+        this.descricao = dados.descricao();
+    }
+
+    public Publicacao(Usuario usuario){
+        this.setUsuario(usuario);
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoPublicacao dados){
+        if (dados.titulo()!=null)
+            this.titulo = dados.titulo();
+
+        if (dados.descricao()!=null)
+            this.descricao = dados.descricao();
+    }
+}
