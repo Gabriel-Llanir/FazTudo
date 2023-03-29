d94ee787eaa87fb4a2b80f287fe06a5f0b10fcb1
Gabriel Llanir <104911512+InsaneXurow10@users.noreply.github.com>
2023-03-29 15:15:14 -0300

    Update V3__create_table_publicacoes.sql

--- src/main/resources/db.migration/V3__create_table_publicacoes.sql
+++ src/main/resources/db.migration/V3__create_table_publicacoes.sql
@@ -1,7 +1,7 @@
 create table publicacoes(
     usuario_id bigint not null auto_increment,
-    titulo varchar(100),
-    descricao varchar(255),
+    titulo varchar(100) not null,
+    descricao varchar(255) not null,
     primary key(usuario_id, titulo),
     constraint fk_publicacoes_usuarios
         foreign key (usuario_id)
