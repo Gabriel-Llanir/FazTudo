0e87c8b974d9b425fc497ee7947eff3dbeeb0f30
Gabriel Llanir <104911512+InsaneXurow10@users.noreply.github.com>
2023-04-19 09:38:55 -0300

    Update PublicacaoRepository.java

--- src/main/java/br/com/fiap/appservico/Domain/Publicacao/PublicacaoRepository.java
+++ src/main/java/br/com/fiap/appservico/Domain/Publicacao/PublicacaoRepository.java
@@ -1,6 +1,9 @@
 package br.com.fiap.appservico.Domain.Publicacao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PublicacaoRepository extends JpaRepository<Publicacao, Long> {
+    Page<Publicacao> findAll(Pageable paginacao);
 }
